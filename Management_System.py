173a77b707be532e6c94155d774f28da8d11c953
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-05-27 14:05:36 -0200

    Update Management_System.py

--- Management_System.py
+++ Management_System.py
@@ -12,25 +12,25 @@ def Database():
     cursor = conn.cursor()
     #creating STUD_REGISTRATION table
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS REGISTRATION (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, NAME TEXT, CAR_MODEL TEXT, CODE TEXT, DATE TEXT, CONTACT TEXT)")
+        "CREATE TABLE IF NOT EXISTS REGISTRATION (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, NAME TEXT, CODE TEXT, DATE TEXT, CONTACT TEXT, PRICE TEXT)")
 
 #defining function for creating GUI Layout
 def DisplayForm():
     #creating window
     display_screen = Tk()
     #setting width and height for window
-    display_screen.geometry("900x400")
+    display_screen.geometry("1200x600")
     #setting title for window
     display_screen.title("Jesse Softwares                                                             https://relaxed-dijkstra-f2b25b.netlify.app")
     global tree
     global SEARCH
-    global name,car_model,code,date,contact
+    global name,code,date,contact,price
     SEARCH = StringVar()
     name = StringVar()
-    car_model = StringVar()
     code = StringVar()
     date = StringVar()
     contact = StringVar()
+    price = StringVar()
     #creating frames for layout
     #topview frame for heading
     TopViewForm = Frame(display_screen, width=600, bd=1, relief=SOLID)
@@ -45,19 +45,19 @@ def DisplayForm():
     MidViewForm = Frame(display_screen, width=600)
     MidViewForm.pack(side=RIGHT)
     #label for heading
-    lbl_text = Label(TopViewForm, text="Sistema de Gestão", font=('verdana', 18), width=600,bg="#1C2833",fg="white")
+    lbl_text = Label(TopViewForm, text="Rubens Amexeira", font=('verdana', 18), width=600,bg="#1C2833",fg="white")
     lbl_text.pack(fill=X)
     #creating registration form in first left frame
     Label(LFrom, text="Nome  ", font=("Arial", 12)).pack(side=TOP)
     Entry(LFrom,font=("Arial",10,"bold"),textvariable=name).pack(side=TOP, padx=10, fill=X)
-    Label(LFrom, text="Modelo de Carro ", font=("Arial", 12)).pack(side=TOP)
-    Entry(LFrom, font=("Arial", 10, "bold"),textvariable=car_model).pack(side=TOP, padx=10, fill=X)
     Label(LFrom, text="Placa ", font=("Arial", 12)).pack(side=TOP)
     Entry(LFrom, font=("Arial", 10, "bold"),textvariable=code).pack(side=TOP, padx=10, fill=X)
     Label(LFrom, text="Data ", font=("Arial", 12)).pack(side=TOP)
     Entry(LFrom, font=("Arial", 10, "bold"),textvariable=date).pack(side=TOP, padx=10, fill=X)
     Label(LFrom, text="Fone ", font=("Arial", 12)).pack(side=TOP)
     Entry(LFrom, font=("Arial", 10, "bold"),textvariable=contact).pack(side=TOP, padx=10, fill=X)
+    Label(LFrom, text="Preço(R$) ", font=("Arial", 12)).pack(side=TOP)
+    Entry(LFrom, font=("Arial", 10, "bold"),textvariable=price).pack(side=TOP, padx=10, fill=X)
     Button(LFrom,text="Salvar",font=("Arial", 10, "bold"),command=register).pack(side=TOP, padx=10,pady=5, fill=X)
 
     #creating search label and entry in second frame
@@ -81,7 +81,7 @@ def DisplayForm():
    #setting scrollbar
     scrollbarx = Scrollbar(MidViewForm, orient=HORIZONTAL)
     scrollbary = Scrollbar(MidViewForm, orient=VERTICAL)
-    tree = ttk.Treeview(MidViewForm,columns=("Id", "Nome", "Modelo de Carro", "Placa","Data","Fone"),
+    tree = ttk.Treeview(MidViewForm,columns=("Id", "Nome", "Placa","Data","Fone", "Preço(R$)"),
                         selectmode="extended", height=100, yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
     scrollbary.config(command=tree.yview)
     scrollbary.pack(side=RIGHT, fill=Y)
@@ -90,10 +90,10 @@ def DisplayForm():
     #setting headings for the columns
     tree.heading('Id', text="Id", anchor=W)
     tree.heading('Nome', text="Nome", anchor=W)
-    tree.heading('Modelo de Carro', text="Modelo de Carro", anchor=W)
     tree.heading('Placa', text="Placa", anchor=W)
     tree.heading('Data', text="Data", anchor=W)
     tree.heading('Fone', text="Fone", anchor=W)
+    tree.heading('Preço(R$)', text="Preço(R$)", anchor=W)
     #setting width of the columns
     tree.column('#0', stretch=NO, minwidth=0, width=0)
     tree.column('#1', stretch=NO, minwidth=0, width=100)
@@ -107,17 +107,18 @@ def register():
     Database()
     #getting form data
     name1=name.get()
-    car_model1=car_model.get()
     code1=code.get()
     date1=date.get()
     contact1=contact.get()
+    price1=price.get()
     #applying empty validation
-    if name1=='' or car_model1==''or code1=='' or date1==''or contact1=='':
-        tkMessageBox.showinfo("Aviso","Preencha o campo vazio!!!")
+    if name1=='' or car_model1==''or code1=='' or date1==''or contact1=='' or price1=='':
+        x=0
+        # tkMessageBox.showinfo("Aviso","Preencha o campo vazio!!!")
     else:
         #execute query
         conn.execute('INSERT INTO REGISTRATION (NAME,CAR_MODEL,CODE,DATE,CONTACT) \
-              VALUES (?,?,?,?,?)',(name1,car_model1,code1,date1,contact1));
+              VALUES (?,?,?,?,?)',(name1,code1,date1,contact1,price1));
         conn.commit()
         tkMessageBox.showinfo("Messagem","Salva com sucesso")
         #refresh table data
@@ -133,10 +134,10 @@ def Reset():
     #clear search text
     SEARCH.set("")
     name.set("")
-    car_model.set("")
     code.set("")
     date.set("")
     contact.set("")
+    price1.set("")
 def Delete():
     #open database
     Database()
